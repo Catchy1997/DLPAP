d5fbcc86de24b4bdfc256cb1f95c57de1b36e892
zhanglin <1003677516@qq.com>
2020-05-19 14:09:51 +0800

    alter the way of getting citations

--- data_process/split_citations.py
+++ data_process/split_citations.py
@@ -3,26 +3,25 @@ import pandas as pd
 from tqdm import tqdm
 
 if __name__ == '__main__':
-    file = "E:/Pythonworkspace/patent/patent_data/PatentsView/uspatentcitation.tsv/uspatentcitation.tsv"
+	file = "E:/Pythonworkspace/patent/patent_data/PatentsView/usapplicationcitation.tsv/usapplicationcitation.tsv"
 
-    with open(file, 'r', encoding='utf-8') as f:
-        for line in tqdm(f, ncols=50):
-            uuid = line.split()[0]
-            patent_id = line.split()[1]
-            citation_id = line.split()[2]
-            date = line.split()[3]
-            name = line.split()[4]
-            kind = line.split()[5]
-            country = line.split()[6]
-            category = line.split()[7]
-            sequence = line.split()[8]
-            
-            year = re.findall(r"([\d]{4})-[\d]{2}-[\d]{2}", date)        
-            if year:
-                filepath = "E:/Pythonworkspace/patent/process_data/citations/" + year[0] + ".csv"
-                
-                dataframe = pd.DataFrame({'patent_id':[patent_id],'citation_id':[citation_id],'date':[date],'category':[category],'country    ':[country    ]})
-                if os.path.exists(filepath):
-                    dataframe.to_csv(filepath, header=0, mode='a', index=False, sep=',')
-                else:
-                    dataframe.to_csv(filepath, mode='a', index=False, sep=',')
+	index = 0
+	with open(file, 'r', encoding='utf-8') as f:
+		for line in tqdm(f, ncols=60):
+			index = index + 1
+			if index > 20065000:
+				date = line.split('\t')[3]
+				year = re.findall(r"([\d]{4})-[\d]{2}-[\d]{2}", date)
+				if year:
+					patent_id = line.split('\t')[1]
+					number = line.split('\t')[-4]
+					country = line.split('\t')[-3]
+					category = line.split('\t')[-2]
+
+					filepath = "E:/Pythonworkspace/patent/process_data/citations_app/" + year[0] + ".csv"
+						
+					dataframe = pd.DataFrame({'patent_id':[patent_id],'application_id':[number],'date':[date],'category':[category],'country':[country]})
+					if os.path.exists(filepath):
+						dataframe.to_csv(filepath, header=0, mode='a', index=False, sep=',')
+					else:
+						dataframe.to_csv(filepath, mode='a', index=False, sep=',')
